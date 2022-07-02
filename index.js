d2f87c3ab1d67c1cdf9e3a58f7b0ebb56a76d3d8
Olivia <oliviabennington@gmail.com>
2022-07-01 22:00:18 -0400

    pseudocode updated

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-// Create a quiz game that asks the user timed questions
-//Questions must be multiple-choice
-//Keep track of user scores
+// Create a quiz game that asks the user timed questions:
+//Show first question text, choices, and remaining time of 10 when start is clicked
+//Count down 30 seconds at a time
+//Show the next question and reset the timer when a correct option is chosen
+//show the next question and reset the timer when an incorrect option is chosen
+//end game when timer runs out on last question
+//show score when game is over and start quiz button
+//store score in localStorage under the key "previous-score"
 
 var questionsArr = [
   {
