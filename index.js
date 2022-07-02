ea01b45dfba68fbf8b59ab8275fa223983166fff
Olivia <oliviabennington@gmail.com>
2022-07-01 21:58:02 -0400

    5 questions

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
 // Create a quiz game that asks the user timed questions
 //Questions must be multiple-choice
 //Keep track of user scores
+
+var questionsArr = [
+  {
+    question: 'Who created JavaScript?',
+    answer: 'Brendan Eich',
+    options: [
+      'Linus Torvalds',
+      'Brendan Eich',
+      'Dan Abramov',
+      'Douglas Crockford',
+    ]
+  },
+//2
+  {
+    question: 'Who created JavaScript?',
+    answer: 'Brendan Eich',
+    options: [
+      'Linus Torvalds',
+      'Brendan Eich',
+      'Dan Abramov',
+      'Douglas Crockford',
+    ]
+  },
+  //3
+  {
+    question: 'Who created JavaScript?',
+    answer: 'Brendan Eich',
+    options: [
+      'Linus Torvalds',
+      'Brendan Eich',
+      'Dan Abramov',
+      'Douglas Crockford',
+    ]
+  },
+  //4
+  {
+    question: 'Who created JavaScript?',
+    answer: 'Brendan Eich',
+    options: [
+      'Linus Torvalds',
+      'Brendan Eich',
+      'Dan Abramov',
+      'Douglas Crockford',
+    ]
+  },
+  //5
+  {
+    question: 'Who created JavaScript?',
+    answer: 'Brendan Eich',
+    options: [
+      'Linus Torvalds',
+      'Brendan Eich',
+      'Dan Abramov',
+      'Douglas Crockford',
+    ]
+  },
+]
